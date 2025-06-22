3b25bc598a8286de6c9a9d8319027b7ee7e61093
Naitian Zhou <naitian@berkeley.edu>
2025-06-22 03:36:43 +0000

    Make data paths configurable with env vars

--- src/fashion/paths.py
+++ src/fashion/paths.py
@@ -1,12 +1,18 @@
 from pathlib import Path
+import os
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR / ".." / "data"
 CHECKPOINT_DIR = DATA_DIR / "checkpoints"
 
-CHICAGO_PATH = DATA_DIR / "ChicagoCorpus"
-LITBANK_PATH = Path("/mnt/data1/corpora/litbank/original/")
+CHICAGO_PATH = Path(os.environ.get("CHICAGO_PATH", DATA_DIR / "ChicagoCorpus"))
+LITBANK_PATH = Path(
+    os.environ.get("LITBANK_PATH", "/mnt/data1/corpora/litbank/original/")
+)
 CONTEMP_LITBANK_PATH = Path(
-    "/mnt/data1/corpora/contemporary_litbank/english/stripped_paratext/"
+    os.environ.get(
+        "CONTEMP_LITBANK_PATH",
+        "/mnt/data1/corpora/contemporary_litbank/english/stripped_paratext/",
+    )
 )
