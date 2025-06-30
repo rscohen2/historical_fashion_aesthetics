5a83a670159b0cff892d45b29996e3ba019dffe2
Naitian Zhou <naitian@berkeley.edu>
2025-06-30 15:13:53 +0000

    Fix slurm

--- slurm/single.sh
+++ slurm/single.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 DATA_PARTITION=$1
+DISTRIBUTED_RANK=$2
 
 sbatch <<EOT
 #!/bin/bash
 
-#SBATCH --job-name=debug
+#SBATCH --job-name=single_adj
 #SBATCH --partition=savio3_gpu
 #SBATCH --account=fc_dbamman
 
-#SBATCH --time=00:05:00
+#SBATCH --time=3:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
@@ -19,8 +20,7 @@ sbatch <<EOT
 #SBATCH --output=/global/scratch/users/naitian/fashion/logs/s-%x.%A_%a.out
 #SBATCH --error=/global/scratch/users/naitian/fashion/logs/s-%x.%A_%a.err
 
-
-export DISTRIBUTED_RANK=1
+export DISTRIBUTED_RANK=$DISTRIBUTED_RANK
 export DISTRIBUTED_WORLD_SIZE=64
 pixi run python -m src.fashion.pipeline.steps.extract_adjectives \
     --noun_mention_dir /global/scratch/users/naitian/fashion/data/pipeline/hathi_all/entity_mentions/$DATA_PARTITION/ \
